f5e9374cae2a14061f6a3a58e4014e6c81fa9592
hariharan-045 <hariharan.shp@gmail.com>
2026-07-10 17:33:54 +0530

    added laptop program OOP'S concept

---
+++ oop's.py/laptopOOP.py
@@ -0,0 +1,16 @@
+class laptop:
+    price = ""
+    processor = "" 
+    ram = ""
+
+hp = laptop()
+dell = laptop()
+lenova = laptop()
+
+hp.price="30000"
+hp.processor="i3.1200u"
+hp.ram="8gp"
+
+print(hp.price)
+print(hp.processor)
+print(hp.ram)
